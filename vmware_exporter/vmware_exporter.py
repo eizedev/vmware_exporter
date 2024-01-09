8c14f5e2c3f3fad4ace80fd3c8daed861c2b582a
eizedev <github@eize.dev>
2024-01-09 16:54:15 +0100

    added possibility to specify different port for host (default 443)

--- vmware_exporter/vmware_exporter.py
+++ vmware_exporter/vmware_exporter.py
@@ -57,6 +57,7 @@ class VmwareCollector():
     def __init__(
             self,
             host,
+            port,
             username,
             password,
             collect_only,
@@ -68,6 +69,7 @@ class VmwareCollector():
     ):
 
         self.host = host
+        self.port = port
         self.username = username
         self.password = password
         self.ignore_ssl = ignore_ssl
@@ -2139,6 +2141,7 @@ class VMWareMetricsResource(Resource):
         self.config = {
             'default': {
                 'vsphere_host': os.environ.get('VSPHERE_HOST'),
+                'vsphere_port': os.environ.get('VSPHERE_PORT', 443),
                 'vsphere_user': os.environ.get('VSPHERE_USER'),
                 'vsphere_password': os.environ.get('VSPHERE_PASSWORD'),
                 'ignore_ssl': get_bool_env('VSPHERE_IGNORE_SSL', False),
@@ -2167,6 +2170,7 @@ class VMWareMetricsResource(Resource):
 
             self.config[section.lower()] = {
                 'vsphere_host': os.environ.get('VSPHERE_{}_HOST'.format(section)),
+                'vsphere_port': os.environ.get('VSPHERE_{}_PORT'.format(section), 443),
                 'vsphere_user': os.environ.get('VSPHERE_{}_USER'.format(section)),
                 'vsphere_password': os.environ.get('VSPHERE_{}_PASSWORD'.format(section)),
                 'ignore_ssl': get_bool_env('VSPHERE_{}_IGNORE_SSL'.format(section), False),
@@ -2226,6 +2230,7 @@ class VMWareMetricsResource(Resource):
 
         collector = VmwareCollector(
             vsphere_host,
+            self.config[section]['vsphere_port'],
             self.config[section]['vsphere_user'],
             self.config[section]['vsphere_password'],
             self.config[section]['collect_only'],
